ffdac01a872c0e62e64857840116ee0035804303
Alexander <shmygarevalexander009@gmail.com>
2023-02-04 06:39:29 +0500

    HomeWork9

---
+++ Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Данная программа находит сумму натуральных элементов в промежутке числовых значений от M до N, которые задаются пользователем.");
+
+int InputNumber(string input)
+{
+Console.WriteLine(input);
+int output = Convert.ToInt32(Console.ReadLine());
+return output;
+}
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+}
+
+int m = InputNumber($"Введите числовое значение - M: "); 
+int n = InputNumber($"Введите числовое значение - N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+Console.Write($"Cумма натуральных элементов в промежутке от {m} до {n} составляет:");
+PrintSum(m, n, temp = 0);
---
+++ Task68/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Данная программа вычисляет функцию Аккермана с помощью рекурсии. Числовые значения M и N для вычисления данной функции задаются пользователем.");
+
+int InputNumber(string input)
+{
+Console.Write(input);
+
+int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+    return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+    return Akkerman(m - 1, 1);
+    }
+    else
+    {
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
+
+int m = InputNumber("Введите числовое значение - M: ");
+int n = InputNumber("Введите числовое значение - N: ");
+int akkermanFunction = Akkerman(m, n);
+Console.Write($"Вычисленная функция Аккермана с помощью рекурсии при введённых пользователем числовых значений: M = {m}; N = {n} равняется: {akkermanFunction}.");
